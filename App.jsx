5d1d6c2c5fc7672bf25d8b1b789f3873854b6948
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-09-23 18:04:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css';
+import Navbar from './components/Navbar'
+import './components/container.css';
+import CurvedIconLayout from './components/container1';
+import Containertwo from './components/container2';
+
+
+function App() {
+  return ( 
+    
+   <>
+    <>
+      <Navbar title="liner" /></>
+      <>
+      <CurvedIconLayout title="head" /></>
+      <>
+        <Containertwo title="body"/></>
+        </>
+        
+
+    
+   
+
+ 
+
+
+
+ );
+}
+
+export default App;
